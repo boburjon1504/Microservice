a1770e314a0576c3c7024ea2ba57a1879622af28
Boburjon Juraboev <bjuraboev@exadel.com>
2025-09-29 16:07:35 +0500

    addded the web project

---
+++ Micorservice.Coupon/Models/Dto/ResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Micorservice.CouponApi.Models.Dto;
+
+public class ResponseDto<TData>
+{
+    public TData? Resule { get; set; }
+
+    public bool IsSuccess { get; set; }
+
+    public string? Message { get; set; }
+}
