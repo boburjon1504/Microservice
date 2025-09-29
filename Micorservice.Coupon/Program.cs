71e1ee503d203f0d611bc44509dd5a2e449c271f
Boburjon Juraboev <bjuraboev@exadel.com>
2025-09-29 14:05:54 +0500

    added coupon apu

---
+++ Micorservice.Coupon/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app
+        .UseSwagger()
+        .UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
