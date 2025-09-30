a779eba78a2b6de034daf7c26aac37121efd08be
Boburjon Juraboev <bjuraboev@exadel.com>
2025-09-30 15:37:45 +0500

    added jwt token generator service

---
+++ Microservice.AuthApi/Services/Interfaces/IJwtTokenGenerator.cs
@@ -0,0 +1,8 @@
+﻿using Microservice.AuthApi.Models;
+
+namespace Microservice.AuthApi.Services.Interfaces;
+
+public interface IJwtTokenGenerator
+{
+    string GenerateToken(ApplicationUser user);
+}
---
+++ Microservice.AuthApi/Services/JwtTokenGenerator.cs
@@ -0,0 +1,40 @@
+﻿using Microservice.AuthApi.Models;
+using Microservice.AuthApi.Services.Interfaces;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Microservice.AuthApi.Services;
+
+public class JwtTokenGenerator(IOptions<JwtOptions> jwtOptions) : IJwtTokenGenerator
+{
+    private JwtOptions _jwtOptions = jwtOptions.Value;
+    public string GenerateToken(ApplicationUser user)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        var key = Encoding.ASCII.GetBytes(_jwtOptions.Secret);
+
+        List<Claim> claims = new List<Claim>
+        {
+            new Claim(JwtRegisteredClaimNames.Name, user.Name),
+            new Claim(JwtRegisteredClaimNames.Email, user.Email),
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+        };
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Audience = _jwtOptions.Audience,
+            Issuer = _jwtOptions.Issuer,
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.Now.AddDays(7),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(token);
+    }
+}
