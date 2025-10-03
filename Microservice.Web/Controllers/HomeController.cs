636f961f80cca96bd11c7e4b71a86ddd34b17af1
Boburjon Juraboev <bjuraboev@exadel.com>
2025-10-03 15:07:12 +0500

    added product details page for web app

--- Microservice.Web/Controllers/HomeController.cs
+++ Microservice.Web/Controllers/HomeController.cs
@@ -1,21 +1,50 @@
 using Microservice.Web.Models;
+using Microservice.Web.Services;
+using Microservice.Web.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Text.Json;
 
 namespace Microservice.Web.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController(IProductService productService) : Controller
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public async  Task<IActionResult> Index()
         {
-            _logger = logger;
+            List<ProductDto>? products = new List<ProductDto>();
+
+            var response = await productService.GetAllProductsAsync();
+
+            if (response is not null && response.IsSuccess)
+            {
+                products = JsonSerializer.Deserialize<List<ProductDto>>(Convert.ToString(response.Result), new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            }
+            else
+            {
+                TempData["error"] = response.Message;
+            }
+
+            return View(products);
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Details(int id)
         {
-            return View();
+            var response = await productService.GetByIdAsync(id);
+
+            if (response is not null && response.IsSuccess)
+            {
+                var product = JsonSerializer.Deserialize<ProductDto>(Convert.ToString(response.Result), new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+                return View(product);
+            }
+            else
+            {
+                TempData["error"] = response.Message;
+            }
+
+            return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Privacy()
--- Microservice.Web/Models/ProductDto.cs
+++ Microservice.Web/Models/ProductDto.cs
@@ -15,4 +15,7 @@ public class ProductDto
     public string CategoryName { get; set; } = string.Empty;
 
     public string ImageUrl { get; set; } = string.Empty;
+
+    [Range(1, 100)]
+    public int Count { get; set; } = 1;
 }
