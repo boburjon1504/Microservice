5a5b71bbe35b2c95d10bfb8a795dc0b0b7fd54bf
Boburjon Juraboev <bjuraboev@exadel.com>
2025-10-03 14:19:11 +0500

    added product update page for web app

--- Microservice.Web/Controllers/ProductController.cs
+++ Microservice.Web/Controllers/ProductController.cs
@@ -4,84 +4,118 @@ using Microsoft.AspNetCore.Mvc;
 using System.Text.Json;
 using System.Threading.Tasks;
 
-namespace Microservice.Web.Controllers
+namespace Microservice.Web.Controllers;
+
+public class ProductController(IProductService productService) : Controller
 {
-    public class ProductController(IProductService productService) : Controller
+    public async Task<IActionResult> ProductIndex()
     {
-        public async Task<IActionResult> ProductIndex()
+        List<ProductDto>? products = new List<ProductDto>();
+
+        var response = await productService.GetAllProductsAsync();
+
+        if (response is not null && response.IsSuccess)
         {
-            List<ProductDto>? products = new List<ProductDto>();
-
-            var response = await productService.GetAllProductsAsync();
-
-            if (response is not null && response.IsSuccess)
-            {
-                products = JsonSerializer.Deserialize<List<ProductDto>>(Convert.ToString(response.Result), new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-            }
-            else
-            {
-                TempData["error"] = response.Message;
-            }
-
-            return View(products);
+            products = JsonSerializer.Deserialize<List<ProductDto>>(Convert.ToString(response.Result), new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+        else
+        {
+            TempData["error"] = response.Message;
         }
 
-        public IActionResult ProductCreate()
+        return View(products);
+    }
+
+    public IActionResult ProductCreate()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> ProductCreate(ProductDto model)
+    {
+        var response = await productService.CreateAsync(model);
+
+        if (response is not null && response.IsSuccess)
         {
-            return View();
+            TempData["success"] = "Product created successfully";
+
+            return RedirectToAction(nameof(ProductIndex));
         }
-
-        [HttpPost]
-        public async Task<IActionResult> ProductCreate(ProductDto model)
+        else
         {
-            var response = await productService.CreateAsync(model);
-
-            if (response is not null && response.IsSuccess)
-            {
-                TempData["success"] = "Product created successfully";
-
-                return RedirectToAction(nameof(ProductIndex));
-            }
-            else
-            {
-                TempData["error"] = response.Message;
-                return View(model);
-            }
+            TempData["error"] = response.Message;
+            return View(model);
         }
+    }
 
-        public async Task<IActionResult> ProductDelete(int id)
+    public async Task<IActionResult> ProductUpdate(int id)
+    {
+        ResponseDto? response = await productService.GetByIdAsync(id);
+
+        if (response is not null && response.IsSuccess)
         {
-            ResponseDto? response = await productService.GetByIdAsync(id);
+            ProductDto? productDto = JsonSerializer.Deserialize<ProductDto>(Convert.ToString(response.Result), new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
 
-            if (response is not null && response.IsSuccess)
-            {
-                ProductDto? productDto = JsonSerializer.Deserialize<ProductDto>(Convert.ToString(response.Result), new JsonSerializerOptions { PropertyNameCaseInsensitive = true});
-
-                return View(productDto);
-            }
-            else
-            {
-                TempData["error"] = response.Message;
-                return NotFound();
-            }
+            return View(productDto);
         }
-
-        [HttpPost]
-        public async Task<IActionResult> ProductDelete(ProductDto model)
+        else
         {
-            var response = await productService.DeleteAsync(model.Id);
+            TempData["error"] = response.Message;
+            return NotFound();
+        }
+    }
 
-            if (response is not null && response.IsSuccess)
-            {
-                TempData["success"] = "Product deleted successfully";
+    [HttpPost]
+    public async Task<IActionResult> ProductUpdate(ProductDto model)
+    {
+        var response = await productService.UpdateAsync(model);
 
-                return RedirectToAction(nameof(ProductIndex));
-            }
-            else
-            {
-                TempData["error"] = response.Message;
-                return View(model);
-            }
+        if (response is not null && response.IsSuccess)
+        {
+            TempData["success"] = "Product updated successfully";
+
+            return RedirectToAction(nameof(ProductIndex));
+        }
+        else
+        {
+            TempData["error"] = response.Message;
+            return View(model);
+        }
+    }
+
+    public async Task<IActionResult> ProductDelete(int id)
+    {
+        ResponseDto? response = await productService.GetByIdAsync(id);
+
+        if (response is not null && response.IsSuccess)
+        {
+            ProductDto? productDto = JsonSerializer.Deserialize<ProductDto>(Convert.ToString(response.Result), new JsonSerializerOptions { PropertyNameCaseInsensitive = true});
+
+            return View(productDto);
+        }
+        else
+        {
+            TempData["error"] = response.Message;
+            return NotFound();
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> ProductDelete(ProductDto model)
+    {
+        var response = await productService.DeleteAsync(model.Id);
+
+        if (response is not null && response.IsSuccess)
+        {
+            TempData["success"] = "Product deleted successfully";
+
+            return RedirectToAction(nameof(ProductIndex));
+        }
+        else
+        {
+            TempData["error"] = response.Message;
+            return View(model);
         }
     }
 }
